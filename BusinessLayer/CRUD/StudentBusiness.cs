27257548d611dcdff84874f9780e91e3d23b7178
Immer Nehemías Guevara Ramos <127710364+Immer14G@users.noreply.github.com>
2023-10-30 22:18:54 -0600

    Add files via upload

---
+++ BusinessLayer/CRUD/StudentBusiness.cs
@@ -0,0 +1,39 @@
+﻿using CommonLayer;
+using DataLayer;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer.CRUD
+{
+    public class StudentBusiness
+    {
+        StudentData studentData = new StudentData();
+
+        public DataTable GetStudents()
+        {
+
+            DataTable studentTable = new DataTable();
+            studentTable = studentData.GetAllStudents();
+
+            return studentTable;
+        }
+        public void AddStudents(Student student)
+        {
+            studentData.AddStudents(student);
+        }
+
+        public void UpdateStudents(Student student)
+        {
+            studentData.UpdateStudents(student);
+        }
+
+        public void DeleteStudents(Student student)
+        {
+            studentData.DeleteStudents(student);
+        }
+    }
+}
